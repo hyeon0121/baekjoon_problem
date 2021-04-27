484ec6e62ae9e0247945a19635db6bfefae3f6c6
2015005050 <dpwl1172@naver.com>
2021-04-27 22:55:50 +0900

    Solve algorithm : sliding window(시간초과)

---
+++ 11003_최솟값찾기.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+int n;
+int arr[1501][1501];
+
+int main(void) {
+    cin >> n;
+    priority_queue<int, vector<int>> pq;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    vector<int> v(n,n-1);
+
+    int cnt = 0;
+    4 0 1 2 3 4
+    3 0 
+    while(cnt < 0) {
+        for (int i = 0; i < n; ++i) {
+            /* code */
+        }
+    }
+
+    cout << answer;
+
+    
+    return 0;
+
+}
